b9ce72c77f542cbff9b403d4e297f35199d327a2
Solomon Yaw Adeklo <sadeklo@st.vvu.edu.gh>
2018-04-05 19:12:17 +0100

    Update index.php file
    Update index.php file

--- admission/index.php
+++ admission/index.php
@@ -13,18 +13,22 @@
 .intro{
   margin: auto;
 }
+body
+{
+    background-color:#283644;
+}
 .black {
   width: 50%;
   float: left;
   background: #283644;
-  height: 2200px;
+  height: 2300px;
 }
 
 .white{
   width: 50%;
   float: right;
   background: #4D727E;
-  height: 2200px    ;
+  height: 2300px    ;
 }
 
 .box{
@@ -157,38 +161,41 @@ footer {
   <img src="upload/git.png" width="200" height="100"  "/><h1 style="color: #e4b119;">Ghana Institute Of Technology(GIT) Online Admission System</h1>
   <iframe  style="padding-left: 400px;"width="560" height="315" src="https://www.youtube.com/embed/kh40ykxaD7s" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
   
-  <p style="color: white; font-size:19px;">The mission of the Ghana Institute of Technology is to advance knowledge and educate students in science, technology,</br>
- and other areas of scholarship that will best serve the nation and the world in the 21st century. We are also driven </br>
-to bring knowledge to bear on the world's great challenges.The Institute is an independent, coeducational, government </br>
-endowed university, organized into five Schools (architecture and planning; engineering; humanities, arts, and social </br>
-sciences; management; and science). It has some 1,000 faculty members, more than 11,000 undergraduate and graduate </br>
-students, and more than 130,000 living alumni.At its founding in 2020, GIT was an educational innovation, a community </br>
-of hands-on problem solvers in love with fundamental science and eager to make Africa and the world a better place. Today, that </br>
-spirit still guides how we educate students on campus and how we shape new digital learning technologies to make GIT </br>
-teaching accessible to millions of learners around the world.GIT's spirit of interdisciplinary exploration has fueled </br>
-many scientific breakthroughs and technological advances. A few examples: the first chemical synthesis of penicillin </br>
-and vitamin A. The development of radar and creation of inertial guidance systems. The invention of magnetic core </br>
-memory, which enabled the development of digital computers. Major contributions to the Human Genome Project. The </br>
-discovery of quarks. The invention of the electronic spreadsheet and of encryption systems that enable e-commerce. </br>
-The creation of GPS. Pioneering 3D printing. The concept of the expanding universe.Current research and education areas </br>
- include digital learning; nanotechnology; sustainable energy, the environment, climate adaptation, and global water </br>
-and food security; Big Data, cybersecurity, robotics, and artificial intelligence; human health, including cancer, HIV,</br>
- autism, Alzheimer's, and dyslexia; biological engineering and CRISPR technology; poverty alleviation; advanced manufacturing;</br>
- and innovation and entrepreneurship.GIT's impact also includes the work of our alumni. One way GIT graduates drive </br>
-progress is by starting companies that deliver new ideas to Africa and the world. A recent study estimates that as of 2065, </br>
-living GIT alumni have launched more than 30,000 active companies, creating 4.6 million jobs and generating roughly </br>
+  <p style="color: white; font-size:19px;">The mission of the Ghana Institute of Technology is to advance knowledge and educate students in science, technology,
+ and other areas of scholarship that will best serve the nation and the world in the 21st century. We are also driven 
+to bring knowledge to bear on the world's great challenges.The Institute is an independent, coeducational, government 
+endowed university, organized into five Schools (architecture and planning; engineering; humanities, arts, and social 
+sciences; management; and science). It has some 1,000 faculty members, more than 11,000 undergraduate and graduate 
+students, and more than 130,000 living alumni.At its founding in 2020, GIT was an educational innovation, a community 
+of hands-on problem solvers in love with fundamental science and eager to make Africa and the world a better place. Today, that 
+spirit still guides how we educate students on campus and how we shape new digital learning technologies to make GIT 
+teaching accessible to millions of learners around the world.GIT's spirit of interdisciplinary exploration has fueled 
+many scientific breakthroughs and technological advances. A few examples: the first chemical synthesis of penicillin 
+and vitamin A. The development of radar and creation of inertial guidance systems. The invention of magnetic core 
+memory, which enabled the development of digital computers. Major contributions to the Human Genome Project. The 
+discovery of quarks. The invention of the electronic spreadsheet and of encryption systems that enable e-commerce. 
+The creation of GPS. Pioneering 3D printing. The concept of the expanding universe.Current research and education areas 
+ include digital learning; nanotechnology; sustainable energy, the environment, climate adaptation, and global water 
+and food security; Big Data, cybersecurity, robotics, and artificial intelligence; human health, including cancer, HIV,
+ autism, Alzheimer's, and dyslexia; biological engineering and CRISPR technology; poverty alleviation; advanced manufacturing;
+ and innovation and entrepreneurship.GIT's impact also includes the work of our alumni. One way GIT graduates drive 
+progress is by starting companies that deliver new ideas to Africa and the world. A recent study estimates that as of 2065, 
+living GIT alumni have launched more than 30,000 active companies, creating 4.6 million jobs and generating roughly 
 $1.9 trillion in annual revenue. Taken together, this "GIT Nation" is equivalent to the 10th-largest economy in the
  world!<br /><br />
- The following are all the list of Courses offered in Ghana Istitute Of Technology:<br /><br />
+ The following are all the list of Courses offered in Ghana Institute Of Technology:<br /><br />
 
 Bsc.Computer Science<br />
 Bsc Information Technology<br />
 Bsc Software Engineering<br />
 Bsc.Computer Engineering<br />
+Bsc. Mathematics<br />
 BSc Artificial Intelligence <br />
 Bsc.Robotics<br />
 BSc Chemical Engineering <br />
 BSc Materials Engineering <br />
+BSc Agricultural technology <br />
+BSc Shipbuilding and Repair <br />
 BSc Architecture  <br />
 BA African and African Diaspora Studies<br />
 BSC Astronomy<br />
@@ -199,18 +206,20 @@ Bsc.Electrical Engineering<br />
 Bsc.Nursing<br />
 MBBS/BSc Medicine<br />
 Bsc.Architecture<br />
+BSc Civil Engineering <br />
 Bsc.Geodetic Engineering<br />
 Bsc. Business Systems Analysis<br />
 
 
  
  </p>
+ 
 
-<a style="padding-left: 100;" href="http://localhost/admission/register.php"><input class="button-three" type="submit" value="Click here to register"></a>
-<a style="padding-left: 100;" href="http://localhost/admission/login.php"><input class="button-three" type="submit" value="Click here to login"></a>
-
+<a style="padding-left: 100;" href="http://ghanainstituteoftechnology.tk/register.php"><input class="button-three" type="submit" value="Click here to register"></a>
+<a style="padding-left: 100;" href="http://ghanainstituteoftechnology.tk/login.php"><input class="button-three" type="submit" value="Click here to login"></a>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4046814.590106445!2d-3.2749614048296114!3d7.8984775235963705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfd75acda8dad6c7%3A0x54d7f230d093d236!2sGhana!5e0!3m2!1sen!2sgh!4v1522951363591" width="1350" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 <footer class="footer">
-  &copy; Copyright 2018. Designed by <a href="https://github.com/solomonyaw" target="_blank" >Solomon Yaw</a> - This is a semester project I did during my studies in Internet Enabled Application Systems Development at Valley View University
+  &copy; Copyright 2018. Designed by <a href="https://github.com/solomonyaw" target="_blank" >Solomon Yaw</a> - This is a semester project I did during my studies in Internet Enabled Application Systems Development at <a style="color:white" href="http://vvu.edu.gh" target="_blank" >Valley View University</a>
 </footer>
 </p>
   </div>
